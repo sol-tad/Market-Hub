3db7afd0f0cb10a411b8e2d03ecbb9aa75a3db2d
solabest <soltad65@gmail.com>
2024-08-03 18:04:45 -0700

    create register page

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
   userInfo: localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
@@ -8,17 +9,21 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  setCredentials: (state, action) => {
-    state.userInfo = action.payload;
-    localStorage.setItem("userInfo", JSON.stringify(action.payload));
-    const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 100;
-    localStorage.setItem("expirationTime", expirationTime);
-  },
-  logout: (state) => {
-    state.userInfo = null;
-    localStorage.clear();
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+
+      const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000; // 30 days
+      localStorage.setItem("expirationTime", expirationTime);
+    },
+    logout: (state) => {
+      state.userInfo = null;
+      localStorage.clear();
+    },
   },
 });
 
 export const { setCredentials, logout } = authSlice.actions;
+
 export default authSlice.reducer;
