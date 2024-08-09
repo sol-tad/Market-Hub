a1fd76b1f32a93aa7991cbbaa4fd42e6aa750758
solabest <soltad65@gmail.com>
2024-08-09 03:29:06 -0700

    create model,route and controller for category

---
+++ backend/routes/categoryRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import { createCategory } from "../controllers/categoryController.js";
+const router = express.Router();
+router.route("/").post(createCategory);
+
+export default router;
