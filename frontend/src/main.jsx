8a47e0b95f06606992d2580695be38b9c3048131
solabest <soltad65@gmail.com>
2024-08-09 05:25:20 -0700

    finish category

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,12 @@ import Profile from "./pages/User/Profile.jsx"
 import PrivateRoute from "./components/PrivateRoute";
 import AdminRoute from "./pages/Admin/AdminRoute.jsx";
 import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
+
+
+
+
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
@@ -26,6 +32,8 @@ const router = createBrowserRouter(
       {/* admin route */}
       <Route path="/admin" element={<AdminRoute />}>
        <Route path="userlist" element={<UserList />}/>
+       <Route path="categorylist" element={<CategoryList />}/>
+
       
       </Route>
 
